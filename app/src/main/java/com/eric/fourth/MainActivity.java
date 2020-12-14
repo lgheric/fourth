e9c4551d939b23aac4c0e899d44d207e0a312ba8
lgheric <saverneo@163.com>
2020-12-15 02:53:21 +0800

    init fourth

---
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -0,0 +1,13 @@
+package com.eric.fourth;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
