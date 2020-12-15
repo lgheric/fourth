b07b5716d7c9f9d2b6f366c36131b45974508015
lgheric <saverneo@163.com>
2020-12-15 16:14:19 +0800

    ColorDrawable,NiewPatchDrawable,ShapeDrawable,GradientDrawable

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,6 +1,35 @@
 package com.eric.fourth;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DiscretePathEffect;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathEffect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.ArcShape;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.PathShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtShow = findViewById(R.id.txt_show);
+        Button btn = findViewById(R.id.btn);
+
+        ColorDrawable drawable = new ColorDrawable(0xffff2200);
+        //txtShow.setBackground(drawable);
+        txtShow.setBackgroundColor(Color.argb(0xff, 0x00, 0x00, 0x00));
+
+        int mycolor = getResources().getColor(R.color.colorPrimary);
+        btn.setBackgroundColor(mycolor);
+
+        int getcolor = Resources.getSystem().getColor(android.R.color.holo_green_light);
+        btn.setBackgroundColor(getcolor);
     }
 }
-
