942a070800760067603eb7be3edc6dc379c0c2d7
lgheric <saverneo@163.com>
2020-12-17 01:14:35 +0800

    TransitionDrawable

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,18 +1,32 @@
 package com.eric.fourth;
 
-import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private AnimationDrawable ad;
-
+    private TransitionDrawable td;
+    private ImageView img_show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        img_show = findViewById(R.id.img_show);
+        td = (TransitionDrawable) img_show.getDrawable();
+        td.startTransition(3000);
+        //你可以可以反过来播放，使用reverseTransition即可~
+        //td.reverseTransition(3000);
+    }
+
+    public void test(View v){
+        td.reverseTransition(3000);
+
     }
 }
