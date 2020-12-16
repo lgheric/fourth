10125730b20cce7d49830ca8ecba412c81f16046
lgheric <saverneo@163.com>
2020-12-16 15:58:20 +0800

    clipDrawblee

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,54 +1,51 @@
 package com.eric.fourth;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ComposePathEffect;
-import android.graphics.CornerPathEffect;
-import android.graphics.DiscretePathEffect;
-import android.graphics.LinearGradient;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PathEffect;
-import android.graphics.RectF;
-import android.graphics.Shader;
-import android.graphics.SweepGradient;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.ArcShape;
-import android.graphics.drawable.shapes.OvalShape;
-import android.graphics.drawable.shapes.PathShape;
-import android.graphics.drawable.shapes.RectShape;
-import android.graphics.drawable.shapes.RoundRectShape;
-import android.graphics.drawable.shapes.Shape;
+import android.annotation.SuppressLint;
+import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar mSeekbar;
+    private TextView mTvShow;
+    private ImageView mImageShow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtShow = findViewById(R.id.txt_show);
-        Button btn = findViewById(R.id.btn);
+        mSeekbar = findViewById(R.id.seekbar);
+        mTvShow = findViewById(R.id.tv_info);
+        mImageShow = findViewById(R.id.iv_show);
 
-        ColorDrawable drawable = new ColorDrawable(0xffff2200);
-        //txtShow.setBackground(drawable);
-        txtShow.setBackgroundColor(Color.argb(0xff, 0x00, 0x00, 0x00));
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int max = seekBar.getMax();
+                double scale = (double) progress / (double) max;
+                ClipDrawable drawable = (ClipDrawable) mImageShow.getBackground();
+                drawable.setLevel((int) (10000 * scale));
+                mTvShow.setText(progress + "");
+            }
 
-        int mycolor = getResources().getColor(R.color.colorPrimary);
-        btn.setBackgroundColor(mycolor);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-        int getcolor = Resources.getSystem().getColor(android.R.color.holo_green_light);
-        btn.setBackgroundColor(getcolor);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
