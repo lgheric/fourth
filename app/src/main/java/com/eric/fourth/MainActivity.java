e25e85ab87844f319538d8ebe08083e7f99c36fc
lgheric <saverneo@163.com>
2020-12-16 17:21:02 +0800

    LayerDrawable

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,51 +1,18 @@
 package com.eric.fourth;
 
-import android.graphics.drawable.ClipDrawable;
-import android.graphics.drawable.RotateDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private RotateDrawable rd;
-    private final Handler handler = new Handler(Looper.myLooper()) {
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what == 0x123) {
-                if (rd.getLevel() >= 10000){
-                    Toast.makeText(MainActivity.this, "转完了~",Toast.LENGTH_LONG).show();
-                }
-                rd.setLevel(rd.getLevel() + 400);
-            }
-        }
-    };
+    private AnimationDrawable ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView img_show = (ImageView) findViewById(R.id.img_show);
-        // 核心实现代码
-        rd = (RotateDrawable) img_show.getDrawable();
-        final Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.sendEmptyMessage(0x123);
-                if (rd.getLevel() >= 10000) {
-                    timer.cancel();
-                }
-            }
-        }, 0, 300);
     }
 }
