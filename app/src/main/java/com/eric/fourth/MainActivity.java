1eb90828721caa72b11f5e8740c01e020a69fa25
lgheric <saverneo@163.com>
2020-12-17 02:14:41 +0800

    LevelListDrawable demo2

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,35 +1,17 @@
 package com.eric.fourth;
 
-import android.graphics.drawable.LevelListDrawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    private LevelListDrawable ld;
+    private Button on,off;
     private ImageView img_show;
-
-    private final Handler handler = new Handler(Looper.myLooper()){
-        public void handleMessage(Message msg){
-            if(msg.what == 0x123){
-                if(ld.getLevel() >10000){
-                    ld.setLevel(0);
-                }
-                img_show.setImageLevel(ld.getLevel()+2000);
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        on = findViewById(R.id.btn_on);
+        off = findViewById(R.id.btn_off);
+
+        on.setOnClickListener(this);
+        off.setOnClickListener(this);
+
+
         img_show = findViewById(R.id.img_show);
-        //td = (TransitionDrawable) img_show.getDrawable();
-        //td.startTransition(3000);
-        //你可以可以反过来播放，使用reverseTransition即可~
-        //td.reverseTransition(3000);
-        ld = (LevelListDrawable) img_show.getDrawable();
         img_show.setImageLevel(0);
 
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                    handler.sendEmptyMessage(0x123);
-            }
-        },0,100);
-    }
-
-    public void test(View v){
-        //td.reverseTransition(3000);
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_on){
+            //设置的level值必须在6-10之间
+            img_show.setImageLevel(10000);
+        }else if (v.getId() == R.id.btn_off){
+            //设置的level值必须在12-20之间
+            img_show.setImageLevel(5000);
+        }
+    }
+
 }
