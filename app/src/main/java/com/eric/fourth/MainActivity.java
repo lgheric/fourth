ed090b3895a8e2be83e5fd072f3724de21598656
lgheric <saverneo@163.com>
2020-12-18 02:42:27 +0800

    simple drawing board

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,16 +1,133 @@
 package com.eric.fourth;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView iv;
+    //原图
+    private Bitmap bitsrc;
+    //拷贝图
+    private Bitmap bitcopy;
+    private Canvas canvas;
+    private Paint paint;
+    private int startX;
+    private int startY;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        iv = (ImageView) findViewById(R.id.iv);
+        setBitmap();
+
+        // 设置触摸侦听
+        iv.setOnTouchListener(new View.OnTouchListener() {
+
+            // 触摸屏幕时，触摸事件产生时，此方法调用
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    // 用户手指摸到屏幕
+                    case MotionEvent.ACTION_DOWN:
+                        startX = (int) event.getX();
+                        startY = (int) event.getY();
+                        break;
+                    // 用户手指正在滑动
+                    case MotionEvent.ACTION_MOVE:
+                        int x = (int) event.getX();
+                        int y = (int) event.getY();
+                        canvas.drawLine(startX, startY, x, y, paint);
+                        // 每次绘制完毕之后，本次绘制的结束坐标变成下一次绘制的初始坐标
+                        startX = x;
+                        startY = y;
+                        iv.setImageBitmap(bitcopy);
+                        break;
+                    // 用户手指离开屏幕
+                    case MotionEvent.ACTION_UP:
+                        break;
+
+                }
+                // true：告诉系统，这个触摸事件由我来处理
+                // false：告诉系统，这个触摸事件我不处理，这时系统会把触摸事件传递给imageview的父节点
+                return true;
+            }
+        });
+
+
     }
 
+    public void setBitmap() {
+        // 加载画画板的背景图
+        bitsrc = BitmapFactory.decodeResource(getResources(), R.drawable.fruits);
+        // 创建图片副本
+        // 1.在内存中创建一个与原图一模一样大小的bitmap对象，创建与原图大小一致的白纸
+        bitcopy = Bitmap.createBitmap(bitsrc.getWidth(), bitsrc.getHeight(),
+                bitsrc.getConfig());
+        // 2.创建画笔对象
+        paint = new Paint();
+        // 3.创建画板对象，把白纸铺在画板上
+        canvas = new Canvas(bitcopy);
+        // 4.开始作画，把原图的内容绘制在白纸上
+        canvas.drawBitmap(bitsrc, new Matrix(), paint);
+        // 5.将绘制的图放入imageview中
+        iv.setImageBitmap(bitcopy);
+    }
 
+    public void red(View view){
+        paint.setColor(Color.RED);
+    }
+    public void green(View view){
+        paint.setColor(Color.GREEN);
+    }
+    public void brush(View view){
+        paint.setStrokeWidth(8);
+    }
+    public void cancel(View view){
+        setBitmap();
+    }
+    public void eraser(View view){
+        paint.setColor(Color.rgb(243,243,243));
+
+        paint.setStrokeWidth(30);
+    }
+
+    public void save(View view){
+        String path = Environment.getExternalStorageDirectory() + "/" + "2.png";
+        File file = new File(path);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bitcopy.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 发送sd卡就绪广播
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_MEDIA_MOUNTED);
+        intent.setData(Uri.fromFile(Environment.getExternalStorageDirectory()));
+        sendBroadcast(intent);
+    }
 }
--- app/src/main/java/com/eric/fourth/MyView.java
+++ app/src/main/java/com/eric/fourth/MyView.java
@@ -1,141 +1,106 @@
 package com.eric.fourth;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class MyView  extends View {
 
-    private Paint mPaint;
-    private Bitmap myBitMap;
-    private RectF myRectF,myRectF2;
-    private Path path;
-    private Paint citePaint;
-    private Paint tmpPaint;
+    private Paint mPaint;  //绘制线条的Path
+    private Path mPath;      //记录用户绘制的Path
+    private Canvas mCanvas;  //内存中创建的Canvas
+    private Bitmap mBitmap;  //缓存绘制的内容
+
+    private int mLastX;
+    private int mLastY;
 
     public MyView(Context context) {
-        super(context);
-        init();
+        super(context);init();
     }
 
-    public MyView(Context context, @Nullable AttributeSet attrs){
-        super(context,attrs);
-        init();
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);init();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
+        super(context, attrs, defStyleAttr);init();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        init();
+        super(context, attrs, defStyleAttr, defStyleRes);init();
+    }
+
+
+    private void init(){
+//        mPath = new Path();
+//        mPaint = new Paint();   //初始化画笔
+//        mPaint.setColor(Color.GREEN);
+//        mPaint.setAntiAlias(true);
+//        mPaint.setDither(true);
+//        mPaint.setStyle(Paint.Style.STROKE);
+//        mPaint.setStrokeJoin(Paint.Join.ROUND); //结合处为圆角
+//        mPaint.setStrokeCap(Paint.Cap.ROUND); // 设置转弯处为圆角
+//        mPaint.setStrokeWidth(20);   // 设置画笔宽度
+//
+//        // 初始化bitmap,Canvas
+//        int width = getMeasuredWidth();
+//        int height = getMeasuredHeight();
+//        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//        mCanvas = new Canvas(mBitmap);
     }
 
     @Override
-    public void onDraw(Canvas canvas){
-        super.onDraw(canvas);
-
-        canvas.drawColor(getResources().getColor(R.color.yellow,null));
-//        canvas.drawCircle(400, 400, 100, mPaint);           //画实心圆
-//        canvas.drawRect(0, 0, 200, 100, mPaint);            //画矩形
-//        canvas.drawRect(200, 200, 400, 300, mPaint);            //画矩形
-//        canvas.drawRect(400,200,600,300, mPaint);            //画矩形
-//        canvas.drawBitmap(myBitMap, 400, 500, mPaint);
-//        canvas.drawArc(myRectF,0,90,true,mPaint);  //绘制弧形区域
-//        canvas.drawRoundRect(myRectF,30,30,mPaint); //画圆角矩形
-//
-//        canvas.drawOval(myRectF2,mPaint); //画椭圆
-//
-//        path.moveTo(1500, 1600); //移动到 坐标10,10
-//        path.lineTo(100, 50);
-//        path.lineTo(200,40);
-//        path.lineTo(300, 20);
-//        path.lineTo(200, 10);
-//        path.lineTo(100, 70);
-//        path.lineTo(50, 40);
-//        path.close();
-//        canvas.drawPath(path,mPaint);
-
-//        canvas.drawText("最喜欢看曹神日狗了~",100,100,mPaint);    //绘制文字
-//
-//        path.moveTo(50,50);
-//        path.lineTo(100, 100);
-//        path.lineTo(200, 200);
-//        path.lineTo(300, 300);
-//        path.close();
-//        canvas.drawTextOnPath("最喜欢看曹神日狗了~", path, 50, 50, mPaint);    //绘制文字
-
-        canvas.translate(canvas.getWidth()/2, 200); //将位置移动画纸的坐标点:150,150
-        canvas.drawCircle(0, 0, 100, mPaint); //画圆圈
-
-        //使用path绘制路径文字
-        canvas.save();
-        canvas.translate(-75, -75);
-        //Path path = new Path();
-        path.addArc(myRectF, -180, 180);
-        //Paint citePaint = new Paint(mPaint);
-        citePaint.setTextSize(14);
-        citePaint.setStrokeWidth(1);
-        canvas.drawTextOnPath("绘制表盘~", path, 28, 0, citePaint);
-        canvas.restore();
-
-        //Paint tmpPaint = new Paint(mPaint); //小刻度画笔对象
-        tmpPaint.setStrokeWidth(1);
-
-        float  y=100;
-        int count = 60; //总刻度数
-
-        for(int i=0 ; i <count ; i++){
-            if(i%5 == 0){
-                canvas.drawLine(0f, y, 0, y+12f, mPaint);
-                canvas.drawText(String.valueOf(i/5+1), -4f, y+25f, tmpPaint);
-
-            }else{
-                canvas.drawLine(0f, y, 0f, y +5f, tmpPaint);
-            }
-            canvas.rotate(360/count,0f,0f); //旋转画纸
-        }
-
-        //绘制指针
-        tmpPaint.setColor(Color.GRAY);
-        tmpPaint.setStrokeWidth(4);
-        canvas.drawCircle(0, 0, 7, tmpPaint);
-        tmpPaint.setStyle(Paint.Style.FILL);
-        tmpPaint.setColor(Color.YELLOW);
-        canvas.drawCircle(0, 0, 5, tmpPaint);
-        canvas.drawLine(0, 10, 0, -65, mPaint);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
-    private void init(){
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);          //抗锯齿
-        mPaint.setColor(getResources().getColor(R.color.purple,null));//画笔颜色
-        mPaint.setStyle(Paint.Style.FILL);  //画笔风格
-        mPaint.setTextSize(36);             //绘制文字大小，单位px
-        mPaint.setStrokeWidth(5);           //画笔粗细
+    //重写该方法，在这里绘图
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawPath();
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+    }
 
-        //不要在自定义View的onMeasure、onLayout、onDraw等方法里面做new对象的操作。
-        //因为实例化对象是会耗性能的，而这几个方法会被多次调用，所以需要将对象作为属性，
-        // 在初始化的时候就实例化好对象，在这些方法里面直接用就行了
-        //
-        myBitMap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+    //绘制线条
+    private void drawPath(){
+        mCanvas.drawPath(mPath, mPaint);
+    }
 
-        myRectF = new RectF(0, 0, 150, 150);
-        //myRectF2 = new RectF(800, 1000, 1000, 1300);
-        path = new Path();
-        citePaint = new Paint(mPaint);
-        tmpPaint = new Paint(mPaint);
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
+        int action = event.getAction();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = x;
+                mLastY = y;
+                mPath.moveTo(mLastX, mLastY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dx = Math.abs(x - mLastX);
+                int dy = Math.abs(y - mLastY);
+                if (dx > 3 || dy > 3)
+                    mPath.lineTo(x, y);
+                mLastX = x;
+                mLastY = y;
+                break;
+        }
+
+        invalidate();
+        return true;
     }
 }
