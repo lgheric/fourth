a7386b496a2551f20f9c9c91410afd08b536e207
lgheric <saverneo@163.com>
2020-12-18 01:36:43 +0800

    Paint,canvas and the path.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,83 +1,16 @@
 package com.eric.fourth;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
-
-    static ByteArrayOutputStream byteOut = null;
-    private Bitmap bitmap = null;
-    private Button btn_cut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_cut = findViewById(R.id.btn_cut);
-        btn_cut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                captureScreen();
-            }
-        });
     }
 
-    public void captureScreen() {
-        Runnable action = new Runnable() {
-            @Override
-            public void run() {
-                final View contentView = getWindow().getDecorView();
-                try{
-                    Log.e("HEHE",contentView.getHeight()+":"+contentView.getWidth());
-                    bitmap = Bitmap.createBitmap(contentView.getWidth(),
-                            contentView.getHeight(), Bitmap.Config.ARGB_4444);
-                    contentView.draw(new Canvas(bitmap));
-                    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteOut);
-                    savePic(bitmap, "sdcard/short.png");
-                }catch (Exception e){e.printStackTrace();}
-                finally {
-                    try{
-                        if (null != byteOut)
-                            byteOut.close();
-                        if (null != bitmap && !bitmap.isRecycled()) {
-//                            bitmap.recycle();
-                            bitmap = null;
-                        }
-                    }catch (IOException e){e.printStackTrace();}
 
-                }
-            }
-        };
-        try {
-            action.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void savePic(Bitmap b, String strFileName) {
-        FileOutputStream fos;
-        try {
-            fos = new FileOutputStream(strFileName);
-            boolean success= b.compress(Bitmap.CompressFormat.PNG, 100, fos);
-            fos.flush();
-            fos.close();
-            if(success)
-                Toast.makeText(MainActivity.this, "截屏成功", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
