cbfd266e27c2392b2f9366206ebd519458b4a038
lgheric <saverneo@163.com>
2020-12-19 15:47:41 +0800

    Canvas translate rotate scale skew save restore saveLayerv restoreToCount

--- app/src/main/java/com/eric/fourth/MyView.java
+++ app/src/main/java/com/eric/fourth/MyView.java
@@ -3,11 +3,15 @@ package com.eric.fourth;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -19,10 +23,12 @@ public class MyView  extends View {
     private Path mPath;      //记录用户绘制的Path
     private Canvas mCanvas;  //内存中创建的Canvas
     private Bitmap mBitmap;  //缓存绘制的内容
+    private Bitmap bmp;  //缓存绘制的内容
 
     private int mLastX;
     private int mLastY;
-
+    private Rect rect;
+    private RectF bounds;
     public MyView(Context context) {
         super(context);init();
     }
@@ -46,10 +52,10 @@ public class MyView  extends View {
         mPaint.setColor(Color.GREEN);
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
-        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStyle(Paint.Style.FILL);
         mPaint.setStrokeJoin(Paint.Join.ROUND); //结合处为圆角
         mPaint.setStrokeCap(Paint.Cap.ROUND); // 设置转弯处为圆角
-        mPaint.setStrokeWidth(20);   // 设置画笔宽度
+        mPaint.setStrokeWidth(5);   // 设置画笔宽度
 
         // 初始化bitmap,Canvas
         int width = 600;//getMeasuredWidth();
@@ -58,6 +64,10 @@ public class MyView  extends View {
         System.out.println("Width:"+width+" Height:"+height);
         mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
         mCanvas = new Canvas(mBitmap);
+        rect = new Rect(50,0,150,50);
+        bmp = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        bounds = new RectF(0, 0, 400, 400);
+
     }
 
     @Override
@@ -68,8 +78,78 @@ public class MyView  extends View {
     //重写该方法，在这里绘图
     @Override
     protected void onDraw(Canvas canvas) {
-        drawPath();
-        canvas.drawBitmap(mBitmap, 0, 0, null);
+        //drawPath();
+        //canvas.drawBitmap(mBitmap, 0, 0, null);
+//        for(int i=0; i < 5; i++) {
+//            canvas.drawCircle(50, 50, 50, mPaint);
+//            canvas.translate(100, 100);
+//        }
+
+//        canvas.translate(200, 200);
+//        for(int i = 0; i < 36;i++){
+//            canvas.rotate(10);
+//            canvas.drawRect(rect, mPaint);
+//        }
+
+//        canvas.drawBitmap(bmp,0,0,mPaint);
+//        canvas.scale(0.8f, 0.8f);
+//        canvas.drawBitmap(bmp, 0, 0, mPaint);
+//        canvas.scale(0.8f, 0.8f);
+//        canvas.drawBitmap(bmp,0,0,mPaint);
+
+
+//        canvas.drawBitmap(bmp,0,0,mPaint);
+//        canvas.translate(200, 200);
+//        canvas.skew(0.26f,0);
+//        canvas.drawBitmap(bmp,0,0,mPaint);
+
+
+        System.out.println("当前层数："+canvas.getSaveCount());
+        canvas.save();
+        System.out.println("当前层数："+canvas.getSaveCount());
+        canvas.translate(400, 400);
+        canvas.drawBitmap(bmp, 0, 0, mPaint);
+        canvas.save();
+        System.out.println("当前层数："+canvas.getSaveCount());
+        canvas.rotate(45);
+        canvas.drawBitmap(bmp, 0, 0, mPaint);
+        canvas.save();
+        System.out.println("当前层数："+canvas.getSaveCount());
+        canvas.rotate(45);
+        canvas.drawBitmap(bmp, 0, 0, mPaint);
+        canvas.save();
+        System.out.println("当前层数："+canvas.getSaveCount());
+        canvas.translate(0, 200);
+        canvas.drawBitmap(bmp, 0, 0, mPaint);
+        canvas.restoreToCount(3);
+        System.out.println("当前层数："+canvas.getSaveCount());
+        canvas.translate(0, 200);
+        canvas.drawBitmap(bmp, 0, 0, mPaint);
+
+//
+//        canvas.restore();
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//        canvas.restore();
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//        canvas.translate(0, 200);
+//        canvas.drawBitmap(bmp, 0, 0, mPaint);
+//        canvas.restore();
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//        canvas.restore();
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//        canvas.restore();
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//        canvas.restore();
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//        System.out.println("当前层数："+canvas.getSaveCount());
+//
+//        canvas.saveLayer(bounds, mPaint);
+//        System.out.println("当前层数："+canvas.getSaveCount());
+        //canvas.drawColor(getResources().getColor(R.color.moss_tide,null));
+        //canvas.drawBitmap(bmp, 200, 200, mPaint);
+//        canvas.restoreToCount(3);
+//        System.out.println("当前层数："+canvas.getSaveCount());
+        //canvas.drawBitmap(bmp, 300, 200, mPaint);
     }
 
     //绘制线条
---
+++ app/src/main/java/com/eric/fourth/XfermodeView.java
@@ -0,0 +1,4 @@
+package com.eric.fourth;
+
+public class XfermodeView {
+}
