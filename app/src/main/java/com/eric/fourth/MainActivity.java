163128b8ea67e327e216a5e8bedd9c58208a46ad
lgheric <saverneo@163.com>
2020-12-21 01:35:01 +0800

    simple demo of AnimationDrawable

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,15 +1,47 @@
 package com.eric.fourth;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_start;
+    private Button btn_stop;
+    private ImageView img_show;
+    private AnimationDrawable anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+        anim = (AnimationDrawable) img_show.getBackground();
+    }
 
+    private void bindViews() {
+        btn_start = findViewById(R.id.btn_start);
+        btn_stop = findViewById(R.id.btn_stop);
+        img_show = findViewById(R.id.img_show);
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_start:
+                anim.start();
+                break;
+            case R.id.btn_stop:
+                anim.stop();
+                break;
+        }
     }
 }
