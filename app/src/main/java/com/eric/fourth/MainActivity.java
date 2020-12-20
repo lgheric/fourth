30f60bba81884d50ccdd8c5252fda45062f545ea
lgheric <saverneo@163.com>
2020-12-21 01:58:11 +0800

    Detailed explanation of various animations,such as AlphaAnimation,ScaleAnimation,TranslateAnimation.RotateAnimation,AnimationSet etc.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,48 +1,77 @@
 package com.eric.fourth;
 
 import android.annotation.SuppressLint;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private FrameView fView;
-    private AnimationDrawable anim = null;
+    private Button btn_alpha;
+    private Button btn_scale;
+    private Button btn_tran;
+    private Button btn_rotate;
+    private Button btn_set;
+    private ImageView img_show;
+    private Animation animation = null;
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+    }
 
-        FrameLayout fly = new FrameLayout(this);
-        setContentView(fly);
+    private void bindViews() {
+        btn_alpha = findViewById(R.id.btn_alpha);
+        btn_scale = findViewById(R.id.btn_scale);
+        btn_tran = findViewById(R.id.btn_tran);
+        btn_rotate = findViewById(R.id.btn_rotate);
+        btn_set = findViewById(R.id.btn_set);
+        img_show = findViewById(R.id.img_show);
 
-        fView = new FrameView(this);
-        fView.setBackgroundResource(R.drawable.anim_zhuan);
-        fView.setVisibility(View.INVISIBLE);
-        anim = (AnimationDrawable) fView.getBackground();
-        fView.setAnim(anim);
-        fly.addView(fView);
-        fly.setOnTouchListener(new View.OnTouchListener() {
-            @SuppressLint("ClickableViewAccessibility")
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                //设置按下时才产生动画效果
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    anim.stop();
-                    float x = event.getX();
-                    float y = event.getY();
-                    fView.setLocation((int) y - 40,(int)x-20);  //View显示的位置
-                    fView.setVisibility(View.VISIBLE);
-                    anim.start();    //开启动画
-                }
-                return false;
-            }
-        });
+        btn_alpha.setOnClickListener(this);
+        btn_scale.setOnClickListener(this);
+        btn_tran.setOnClickListener(this);
+        btn_rotate.setOnClickListener(this);
+        btn_set.setOnClickListener(this);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_alpha:
+                animation = AnimationUtils.loadAnimation(this,
+                        R.anim.anim_alpha);
+                img_show.startAnimation(animation);
+                break;
+            case R.id.btn_scale:
+                animation = AnimationUtils.loadAnimation(this,
+                        R.anim.anim_scale);
+                img_show.startAnimation(animation);
+                break;
+            case R.id.btn_tran:
+                animation = AnimationUtils.loadAnimation(this,
+                        R.anim.anim_translate);
+                img_show.startAnimation(animation);
+                break;
+            case R.id.btn_rotate:
+                animation = AnimationUtils.loadAnimation(this,
+                        R.anim.anim_rotate);
+                img_show.startAnimation(animation);
+                break;
+            case R.id.btn_set:
+                animation = AnimationUtils.loadAnimation(this,
+                        R.anim.anim_set);
+                img_show.startAnimation(animation);
+                break;
+        }
     }
 }
