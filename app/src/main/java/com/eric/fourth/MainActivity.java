15ea7c2f60ab8782084c55afc60ee37144809baf
lgheric <saverneo@163.com>
2020-12-21 01:44:02 +0800

    Play the frame animation in the specified place

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -3,45 +3,46 @@ package com.eric.fourth;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private Button btn_start;
-    private Button btn_stop;
-    private ImageView img_show;
-    private AnimationDrawable anim;
+    private FrameView fView;
+    private AnimationDrawable anim = null;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        bindViews();
-        anim = (AnimationDrawable) img_show.getBackground();
-    }
 
-    private void bindViews() {
-        btn_start = findViewById(R.id.btn_start);
-        btn_stop = findViewById(R.id.btn_stop);
-        img_show = findViewById(R.id.img_show);
-        btn_start.setOnClickListener(this);
-        btn_stop.setOnClickListener(this);
-    }
+        FrameLayout fly = new FrameLayout(this);
+        setContentView(fly);
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_start:
-                anim.start();
-                break;
-            case R.id.btn_stop:
-                anim.stop();
-                break;
-        }
+        fView = new FrameView(this);
+        fView.setBackgroundResource(R.drawable.anim_zhuan);
+        fView.setVisibility(View.INVISIBLE);
+        anim = (AnimationDrawable) fView.getBackground();
+        fView.setAnim(anim);
+        fly.addView(fView);
+        fly.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //设置按下时才产生动画效果
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    anim.stop();
+                    float x = event.getX();
+                    float y = event.getY();
+                    fView.setLocation((int) y - 40,(int)x-20);  //View显示的位置
+                    fView.setVisibility(View.VISIBLE);
+                    anim.start();    //开启动画
+                }
+                return false;
+            }
+        });
     }
 }
