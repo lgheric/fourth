286c10446ba8025794895b3988b1cccc6bfed051
lgheric <saverneo@163.com>
2020-12-20 17:29:19 +0800

    The LightingColorFilter

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -3,74 +3,62 @@ package com.eric.fourth;
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.LightingColorFilter;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView img_meizi;
-    private SeekBar sb_hue;
-    private SeekBar sb_saturation;
-    private SeekBar sb_lum;
-    private final static int MAX_VALUE = 255;
-    private final static int MID_VALUE = 127;
-    private float mHue = 0.0f;
-    private float mStauration = 1.0f;
-    private float mLum = 1.0f;
+    private EditText edit_mul;
+    private EditText edit_add;
+    private Button btn_change;
     private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.pre6);
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.pre9);
         bindViews();
     }
 
     private void bindViews() {
         img_meizi = findViewById(R.id.img_meizi);
-        sb_hue = findViewById(R.id.sb_hue);
-        sb_saturation = findViewById(R.id.sb_saturation);
-        sb_lum = findViewById(R.id.sb_lum);
+        edit_mul = findViewById(R.id.edit_mul);
+        edit_add = findViewById(R.id.edit_add);
+        btn_change = findViewById(R.id.btn_change);
 
-        img_meizi.setImageBitmap(mBitmap);
-        sb_hue.setMax(MAX_VALUE);
-        sb_hue.setProgress(MID_VALUE);
-        sb_saturation.setMax(MAX_VALUE);
-        sb_saturation.setProgress(MID_VALUE);
-        sb_lum.setMax(MAX_VALUE);
-        sb_lum.setProgress(MID_VALUE);
+        btn_change.setOnClickListener(this);
 
-        sb_hue.setOnSeekBarChangeListener(this);
-        sb_saturation.setOnSeekBarChangeListener(this);
-        sb_lum.setOnSeekBarChangeListener(this);
+    }
 
+
+    private Bitmap ProcessImage(Bitmap bp,int mul,int add){
+        Bitmap bitmap = Bitmap.createBitmap(bp.getWidth(),bp.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColorFilter(new LightingColorFilter(mul,add));
+        canvas.drawBitmap(bp,0,0,paint);
+        return bitmap;
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        switch (seekBar.getId()) {
-            case R.id.sb_hue:
-                mHue = (progress - MID_VALUE) * 1.0F / MID_VALUE * 180;
-                break;
-            case R.id.sb_saturation:
-                mStauration = progress * 1.0F / MID_VALUE;
-                break;
-            case R.id.sb_lum:
-                mLum = progress * 1.0F / MID_VALUE;
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_change:
+                int mul = Integer.parseInt(edit_mul.getText().toString());
+                int add = Integer.parseInt(edit_add.getText().toString());
+                img_meizi.setImageBitmap(ProcessImage(mBitmap,mul,add));
                 break;
         }
-        img_meizi.setImageBitmap(ImageHelper.handleImageEffect(mBitmap, mHue, mStauration, mLum));
     }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {}
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {}
 }
-
