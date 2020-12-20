d379fe18f826baba509937f9c1ece27aa32cbed5
lgheric <saverneo@163.com>
2020-12-21 02:04:07 +0800

    pop up the login button from the bottom of the app.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,77 +1,30 @@
 package com.eric.fourth;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private Button btn_alpha;
-    private Button btn_scale;
-    private Button btn_tran;
-    private Button btn_rotate;
-    private Button btn_set;
-    private ImageView img_show;
-    private Animation animation = null;
-
+public class MainActivity extends AppCompatActivity {
+    private LinearLayout start_ctrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindViews();
-    }
-
-    private void bindViews() {
-        btn_alpha = findViewById(R.id.btn_alpha);
-        btn_scale = findViewById(R.id.btn_scale);
-        btn_tran = findViewById(R.id.btn_tran);
-        btn_rotate = findViewById(R.id.btn_rotate);
-        btn_set = findViewById(R.id.btn_set);
-        img_show = findViewById(R.id.img_show);
-
-        btn_alpha.setOnClickListener(this);
-        btn_scale.setOnClickListener(this);
-        btn_tran.setOnClickListener(this);
-        btn_rotate.setOnClickListener(this);
-        btn_set.setOnClickListener(this);
-
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_alpha:
-                animation = AnimationUtils.loadAnimation(this,
-                        R.anim.anim_alpha);
-                img_show.startAnimation(animation);
-                break;
-            case R.id.btn_scale:
-                animation = AnimationUtils.loadAnimation(this,
-                        R.anim.anim_scale);
-                img_show.startAnimation(animation);
-                break;
-            case R.id.btn_tran:
-                animation = AnimationUtils.loadAnimation(this,
-                        R.anim.anim_translate);
-                img_show.startAnimation(animation);
-                break;
-            case R.id.btn_rotate:
-                animation = AnimationUtils.loadAnimation(this,
-                        R.anim.anim_rotate);
-                img_show.startAnimation(animation);
-                break;
-            case R.id.btn_set:
-                animation = AnimationUtils.loadAnimation(this,
-                        R.anim.anim_set);
-                img_show.startAnimation(animation);
-                break;
-        }
+        start_ctrl = findViewById(R.id.start_ctrl);
+        //设置动画，从自身位置的最下端向上滑动了自身的高度，持续时间为500ms
+        final TranslateAnimation ctrlAnimation = new TranslateAnimation(
+                TranslateAnimation.RELATIVE_TO_SELF, 0, TranslateAnimation.RELATIVE_TO_SELF, 0,
+                TranslateAnimation.RELATIVE_TO_SELF, 1, TranslateAnimation.RELATIVE_TO_SELF, 0);
+        ctrlAnimation.setDuration(500l);     //设置动画的过渡时间
+        start_ctrl.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                start_ctrl.setVisibility(View.VISIBLE);
+                start_ctrl.startAnimation(ctrlAnimation);
+            }
+        }, 2000);
     }
 }
