4d3ba9a0221c7c4d7651e96e6384d472462daf59
lgheric <saverneo@163.com>
2020-12-21 20:37:33 +0800

    PowerManager, did not see the expected effect.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,89 +1,35 @@
 package com.eric.fourth;
 
 import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.TimePickerDialog;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Calendar;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    private Button btn_set;
-    private Button btn_cancel;
-    private AlarmManager alarmManager;
-    private PendingIntent pi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindViews();
-    }
 
-    private void bindViews() {
-        btn_set = (Button) findViewById(R.id.btn_set);
-        btn_cancel = (Button) findViewById(R.id.btn_cancel);
-        Button start_activity = (Button) findViewById(R.id.start_activity);
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-
-        Intent intent = new Intent(MainActivity.this, ClockActivity.class);
-        pi = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-
-        btn_set.setOnClickListener(this);
-        btn_cancel.setOnClickListener(this);
-         start_activity.setOnClickListener(this);
-
+        Button btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(this);
     }
 
 
-    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_set:
-                Calendar currentTime = Calendar.getInstance();
-                new TimePickerDialog(MainActivity.this, 0,
-                        new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker view,
-                                                  int hourOfDay, int minute) {
-                                //设置当前时间
-                                Calendar c = Calendar.getInstance();
-                                c.setTimeInMillis(System.currentTimeMillis());
-                                // 根据用户选择的时间来设置Calendar对象
-                                c.set(Calendar.HOUR, hourOfDay);
-                                c.set(Calendar.MINUTE, minute);
-                                // ②设置AlarmManager在Calendar对应的时间启动Activity
-                                alarmManager.set(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pi);
-                                Log.e("HEHE",c.getTimeInMillis()+"");   //这里的时间是一个unix时间戳
-                                // 提示闹钟设置完毕:
-                                Toast.makeText(MainActivity.this, "闹钟设置完毕~"+ c.getTimeInMillis(),
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }, currentTime.get(Calendar.HOUR_OF_DAY), currentTime
-                        .get(Calendar.MINUTE), false).show();
-                btn_cancel.setVisibility(View.VISIBLE);
-                break;
-            case R.id.btn_cancel:
-                alarmManager.cancel(pi);
-                btn_cancel.setVisibility(View.GONE);
-                Toast.makeText(MainActivity.this, "闹钟已取消", Toast.LENGTH_SHORT)
-                        .show();
-            case R.id.start_activity:
-                Intent intent = new Intent(MainActivity.this, ClockActivity.class);
-                startActivity(intent);
-                //finish();
-                break;
+        if(v.getId()==R.id.btn_start) {
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            @SuppressLint("InvalidWakeLockTag") PowerManager.WakeLock w1 = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "MyTag");
+            w1.acquire(60*1000L /*1 minutes*/);
+            //在这个过程,屏幕会保持光亮!
+            w1.release();
         }
+
     }
 }
