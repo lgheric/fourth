d76dcb52ab53d8bd93dfb7e85b15660919067ee5
lgheric <saverneo@163.com>
2020-12-24 15:38:08 +0800

    Load XML layout dynamically.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -3,7 +3,9 @@ package com.eric.fourth;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
@@ -14,26 +16,32 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
-        Button btnOne = new Button(this);
-        btnOne.setText("我是动态添加的按钮");
-        btnOne.setTextColor(Color.BLUE);
-
-        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-        lp.addRule(RelativeLayout.VISIBLE);
-
-        //RelativeLayout rly = (RelativeLayout) findViewById(R.id.RelativeLayout1);
-        //rly.addView(btnOne,lp2);
+        setContentView(R.layout.activity_main);
 
 
-        LayoutInflater inflater = LayoutInflater.from(this);
-        RelativeLayout rly = (RelativeLayout) inflater.inflate(R.layout.activity_main, null).findViewById(R.id.RelativeLayout1);
-        rly.addView(btnOne,lp);
+        //1,获取LayoutInflater 对象
+        final LayoutInflater inflater = LayoutInflater.from(this);
+
+        //2,获得外部容器对象
+        final RelativeLayout rly = (RelativeLayout) findViewById(R.id.RelativeLayout1);
+
+        //给外部容器里的button对象添加监听事件
+        Button btnLoad = findViewById(R.id.btnLoad);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获得要添加的布局对象
+                LinearLayout ly = (LinearLayout) inflater.inflate(R.layout.inflate, null, false).findViewById(R.id.ly_inflate);
+
+                //设置加载布局的大小
+                RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                //设置加载布局的位置
+                lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+                rly.addView(ly,lp);
 
 
-        setContentView(rly);
+            }
+        });
 
     }
 
