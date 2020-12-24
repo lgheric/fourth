c2ab0ed978d475b6f75be2ad365515bda8aefe5c
lgheric <saverneo@163.com>
2020-12-25 00:32:59 +0800

    Check which sensors my phone supports.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,71 +1,64 @@
 package com.eric.fourth;
 
-import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.app.WallpaperManager;
-import android.content.Intent;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private Button btn_on;
-    private Button btn_off;
-    private Button btn_clean;
-    private AlarmManager aManager;
-    private PendingIntent pi;
+    private TextView txt_show;
+    private SensorManager sm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //①获得AlarmManager对象:
-        aManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        //②指定要启动的Service,并指明动作是Servce:
-        Intent intent = new Intent(MainActivity.this, WallPaperService.class);
-        pi = PendingIntent.getService(MainActivity.this, 0, intent, 0);
-        bindViews();
-    }
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        txt_show = (TextView) findViewById(R.id.txt_show);
 
-    private void bindViews() {
-        btn_on = (Button) findViewById(R.id.btn_on);
-        btn_off = (Button) findViewById(R.id.btn_off);
-        btn_clean = (Button) findViewById(R.id.btn_clean);
-        btn_on.setOnClickListener(this);
-        btn_off.setOnClickListener(this);
-        btn_clean.setOnClickListener(this);
-    }
+        List<Sensor> allSensors = sm.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder sb = new StringBuilder();
 
-    @SuppressLint({"NonConstantResourceId", "ShortAlarm"})
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_on:
-                aManager.setRepeating(AlarmManager.RTC_WAKEUP, 0, 3000, pi);
-                btn_on.setEnabled(false);
-                btn_off.setEnabled(true);
-                Toast.makeText(MainActivity.this, "自动更换壁纸设置成功", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.btn_off:
-                btn_on.setEnabled(true);
-                btn_off.setEnabled(false);
-                aManager.cancel(pi);
-                break;
-            case R.id.btn_clean:
-                try {
-                    WallpaperManager.getInstance(getApplicationContext()).clear();
-                    Toast.makeText(MainActivity.this, "清除壁纸成功~", Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
+        sb.append("此手机有").append(allSensors.size()).append("个传感器，分别有：\n\n");
+        for(Sensor s:allSensors){
+            switch (s.getType()){
+                case Sensor.TYPE_ACCELEROMETER:
+                    sb.append(s.getType()).append(" 加速度传感器(Accelerometer sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_GYROSCOPE:
+                    sb.append(s.getType()).append(" 陀螺仪传感器(Gyroscope sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_LIGHT:
+                    sb.append(s.getType()).append(" 光线传感器(Light sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    sb.append(s.getType()).append(" 磁场传感器(Magnetic field sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_ORIENTATION:
+                    //SensorManager.getOrientation();
+                    sb.append(s.getType()).append(" 方向传感器(Orientation sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_PRESSURE:
+                    sb.append(s.getType()).append(" 气压传感器(Pressure sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_PROXIMITY:
+                    sb.append(s.getType()).append(" 距离传感器(Proximity sensor)").append("\n");
+                    break;
+                case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                    sb.append(s.getType()).append(" 温度传感器(Temperature sensor)").append("\n");
+                    break;
+                default:
+                    sb.append(s.getType()).append(" 其他传感器").append("\n");
+                    break;
+            }
+            sb.append("设备名称：").append(s.getName()).append("\n 设备版本：").append(s.getVersion()).append("\n 供应商：").append(s.getVendor()).append("\n\n");
         }
+        txt_show.setText(sb.toString());
     }
 }
