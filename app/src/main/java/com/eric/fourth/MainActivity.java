2a7c0815116a8cdc33fd02a1da8aefa19fb64efa
lgheric <saverneo@163.com>
2020-12-24 15:23:19 +0800

    Add componets to layout dynamically. Way two.

--- app/src/main/java/com/eric/fourth/MainActivity.java
+++ app/src/main/java/com/eric/fourth/MainActivity.java
@@ -1,5 +1,6 @@
 package com.eric.fourth;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.Button;
@@ -13,16 +14,27 @@ public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         Button btnOne = new Button(this);
         btnOne.setText("我是动态添加的按钮");
+        btnOne.setTextColor(Color.BLUE);
 
-        RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        lp2.addRule(RelativeLayout.CENTER_IN_PARENT);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+        lp.addRule(RelativeLayout.VISIBLE);
 
-        RelativeLayout rly = (RelativeLayout) findViewById(R.id.RelativeLayout1);
-        rly.addView(btnOne,lp2);
+        //RelativeLayout rly = (RelativeLayout) findViewById(R.id.RelativeLayout1);
+        //rly.addView(btnOne,lp2);
+
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        RelativeLayout rly = (RelativeLayout) inflater.inflate(R.layout.activity_main, null).findViewById(R.id.RelativeLayout1);
+        rly.addView(btnOne,lp);
+
+
+        setContentView(rly);
+
     }
 
 }
